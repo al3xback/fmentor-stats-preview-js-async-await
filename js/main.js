84ae5894db313558a7db2e52ffdfa10e8fa45c8f
alex <al3xback@gmail.com>
2024-01-20 14:04:29 +0700

    added first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import { createHeaderEl, createMainEl, createFooterEl } from './util.js';
+
+/* initApp */
+const body = document.body;
+
+async function initApp() {
+	try {
+		const headerEl = await createHeaderEl();
+		const mainEl = await createMainEl();
+		const footerEl = await createFooterEl();
+
+		body.appendChild(headerEl);
+		body.appendChild(mainEl);
+		body.appendChild(footerEl);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+initApp();
